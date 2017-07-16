89c5cb4d968174a69f4937ac1aa64bc0e065d68f
Stephen Dean <stephendean69@yahoo.in>
2017-07-16 16:10:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,6 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
-        return {}
     result = req.get("result")
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
